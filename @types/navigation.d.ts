4d3da77f8bb839919854cd9a9a4ed4870084765c
lcparada <125614063+lcparada@users.noreply.github.com>
2023-04-10 15:55:07 -0300

    chore: add types

---
+++ @types/navigation.d.ts
@@ -0,0 +1,14 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Login: undefined;
+      Register: undefined;
+      Home: undefined;
+      AppRoutes: undefined;
+      RootRoutes: undefined;
+      AuthRoutes: undefined;
+      Information: undefined;
+      EditProfile: undefined;
+    }
+  }
+}
